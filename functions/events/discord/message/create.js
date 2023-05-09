6ceeafe4f936d70c2a9a6af7b03abb6f3eb07544
AZapie <67051781+AZapie@users.noreply.github.com>
2023-05-09 14:51:09 -0500

    Updated Create.JS
    Includes the Nell Bonker instance where if a user named "Sunella" sends a message there is a 2% chance that it'll be automatically reacted to with an orange dodgeball

--- functions/events/discord/message/create.js
+++ functions/events/discord/message/create.js
@@ -1,19 +1,23 @@
 // authenticates API stuff
 const lib = require('lib')({token: process.env.STDLIB_SECRET_TOKEN});
 
-//code checks for the message "!status" and only runs if it exists
-
+// If !status is executed in the chat by anyone the status is updated to "watching bengalbots"
 if(context.params.event.content.includes("!status")) {
-
   lib.discord.users['@0.2.1'].me.status.update({
     activity_name: `🐯BengalBots🐯`,
     activity_type: 'WATCHING',
     status: 'ONLINE'
   });
+}
 
-  //message executed to signify the status was set
-  return lib.discord.channels['@0.2.0'].messages.create({
-    channel_id: '${context.params.event.channel_id}',
-    content: 'Status has been set.'
-  })
+
+//The Nell Bonker
+if(context.params.event.author.username == ('Moonella')) {
+  if((Math.floor(Math.random() * 100)) <= 2) {
+    let result = await lib.discord.channels['@0.3.4'].messages.reactions.create({
+      emoji: `<:dodgeball:1104215620452622377>`,
+      message_id: `${context.params.event.id}`,
+      channel_id: `${context.params.event.channel_id}` 
+    });
+  }
 }
